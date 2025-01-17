9e074c309a08bcd1d897c11535cc03c5db050c9e
Pope Julius II <PopeJuliusII@PapalStates>
2025-01-17 23:37:31 +0000

    Completed Problem 2235.

---
+++ 2235. Add Two Integers/2235. Add Two Integers.py
@@ -0,0 +1,19 @@
+"""
+Answer to: https://leetcode.com/problems/add-two-integers/
+"""
+
+
+def solution_1(num1: int, num2: int) -> int:
+    """
+    This is the optimal solution to the problem.
+    Both time complexity and space complexity are O(1).
+    It is just addition.
+
+    Args:
+        num1 (int): The first number.
+        num2 (int): The second number.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return num1 + num2
